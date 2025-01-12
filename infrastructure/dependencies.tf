eddc1e33fde2d1120f9eda249104b621b351a006
Ranyel <danielpintilie96@gmail.com>
2025-01-12 18:34:26 +0200

    parse vars from tf output

--- infrastructure/dependencies.tf
+++ infrastructure/dependencies.tf
@@ -1,11 +1,1 @@
-data "aws_ecr_repository" "this" {
-  name = "node-web"  # Replace with your ECR repository name
-}
-
 data "aws_availability_zones" "available" {}
-
-# Step 3: Output the Image URI
-output "ecr_image_uri" {
-  description = "The URI of the image in the ECR repository"
-  value       = "${data.aws_ecr_repository.this.repository_url}:latest"
-}
--- infrastructure/logs.tf
+++ infrastructure/logs.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "cb_log_group" {
-  name              = "/ecs/web-app"
+  name              = "/ecs/${var.container_name}"
   retention_in_days = 30
 
   tags = {
